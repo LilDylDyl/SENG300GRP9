6ba17967428f5fe6cc3337de2e3e0b079f2bd7ae
reeyanmaknojiya <47335900+reeyanmaknojiya@users.noreply.github.com>
2020-04-02 17:12:33 -0600

    Created new method, other changes

--- Appointment.java
+++ Appointment.java
@@ -37,9 +37,10 @@ public class Appointment{
     	int month = c.get(Calendar.MONTH);
         List<String> next12monthsList = new ArrayList<String>();
         String[] monthNames = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        next12monthsList.add("--Choose month--");
         for (int i = month; i < monthNames.length; i++) {
         	String nextMonth = monthNames[i];
-        	next12monthsList.add(monthNames[i]);
+        	next12monthsList.add(monthNames[i]);	
         }
         return next12monthsList;
     }
@@ -66,6 +67,49 @@ public class Appointment{
         		}
         	}
     	}
-		return listOfDates;	
+    	else if ((nameOfMonth == "April") || (nameOfMonth == "June") || (nameOfMonth == "September") || (nameOfMonth == "November")){
+    		String[] totalDates = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"};
+    		if ((nameOfMonth == monthNames[month])) {
+    			int todaysDate = c.get(Calendar.DATE);
+        		for (int i = todaysDate; i < totalDates.length; i++) {
+                	String nextDay = totalDates[i];
+                	listOfDates.add(totalDates[i]);
+        		}
+    		}
+        	else {
+        		for (int i = 0; i < totalDates.length; i++) {
+                	String nextDay = totalDates[i];
+                	listOfDates.add(totalDates[i]);
+        		}
+        	}
+    	}
+    	//Case when month is February
+    	else {
+    		String[] totalDates = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28"};
+    		if ((nameOfMonth == monthNames[month])) {
+    			int todaysDate = c.get(Calendar.DATE);
+        		for (int i = todaysDate; i < totalDates.length; i++) {
+                	String nextDay = totalDates[i];
+                	listOfDates.add(totalDates[i]);
+        		}
+    		}
+        	else {
+        		for (int i = 0; i < totalDates.length; i++) {
+                	String nextDay = totalDates[i];
+                	listOfDates.add(totalDates[i]);
+        		}
+        	}
+    	}
+    	List<String> listOfDates2 = new ArrayList<String>();
+    	listOfDates2.add("--Choose date--");
+    	listOfDates2.addAll(listOfDates);
+		return listOfDates2;
+    }
+    
+    public static List<String> getTime() {
+    	List<String> timeList = new ArrayList<String>();
+        String[] timings = {"9:00 AM", "9:30 AM", "10:00 AM", "10:30 AM", "11:00 AM", "11:30 AM", "12:00 PM", "12:30 PM", "1:00 PM", "1:30 PM", "2:00 PM", "2:30 PM", "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM"};
+        Collections.addAll(timeList, timings);
+        return timeList;
     }
 }
