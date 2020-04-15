b975e8728adee1926ec5084e8ab8c4ccadabef5f
reeyanmaknojiya <47335900+reeyanmaknojiya@users.noreply.github.com>
2020-04-14 23:32:35 -0600

    Update MainPagePatient.java

--- MainPagePatient.java
+++ MainPagePatient.java
@@ -8,12 +8,15 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class MainPagePatient extends JFrame {
 
 	private JPanel contentPane;
+	public static int balanceDue = 0;
 
 	/**
 	 * Launch the application.
@@ -65,14 +68,6 @@ public class MainPagePatient extends JFrame {
 		btnNewButton_1.setBounds(242, 188, 188, 29);
 		contentPane.add(btnNewButton_1);
 		
-		JButton btnNewButton_2 = new JButton("Edit appointment");
-		btnNewButton_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton_2.setBounds(242, 238, 188, 29);
-		contentPane.add(btnNewButton_2);
-		
 		JButton btnNewButton_3 = new JButton("Cancel appointment");
 		btnNewButton_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -80,7 +75,7 @@ public class MainPagePatient extends JFrame {
 				cancelAppointmentPatient.setVisible(true);
 			}
 		});
-		btnNewButton_3.setBounds(242, 287, 188, 29);
+		btnNewButton_3.setBounds(242, 238, 188, 29);
 		contentPane.add(btnNewButton_3);
 		
 		JButton btnNewButton_4 = new JButton("More information");
@@ -90,7 +85,7 @@ public class MainPagePatient extends JFrame {
 				moreInformation.setVisible(true);
 			}
 		});
-		btnNewButton_4.setBounds(242, 337, 188, 29);
+		btnNewButton_4.setBounds(242, 287, 188, 29);
 		contentPane.add(btnNewButton_4);
 		
 		JLabel lblNewLabel = new JLabel("You can manage your appointments using the options below!");
@@ -103,6 +98,27 @@ public class MainPagePatient extends JFrame {
 		lblNewLabel_1.setFont(new Font("Serif", Font.ROMAN_BASELINE, 20));
 		contentPane.add(lblNewLabel_1);
 		
+		JButton btnNewButton_2 = new JButton("Log out");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(contentPane, "You are successfully logged out");
+				LoginPage loginPage = new LoginPage();
+				loginPage.setVisible(true);
+			}
+		});
+		btnNewButton_2.setBounds(574, 0, 92, 29);
+		contentPane.add(btnNewButton_2);
+		
+		JLabel lblNewLabel_2 = new JLabel("Balance due:");
+		lblNewLabel_2.setBounds(6, 5, 78, 16);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("$" + Integer.toString(balanceDue));
+		lblNewLabel_3.setBounds(91, 5, 61, 16);
+		contentPane.add(lblNewLabel_3);
+	}
+	public static void setBalanceDue(int balance) {
+		balanceDue += balance;
 	}
 
 }
